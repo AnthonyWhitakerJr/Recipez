c2ec5572c00f0a90f1cfc95d260c029acb0e8a1e
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-07-31 23:34:54 -0400

    Data model & table view stubs.

--- Recipez/ViewController.swift
+++ Recipez/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
 }
 
